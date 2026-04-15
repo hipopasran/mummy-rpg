e05944e61bbfd77146f05db61a246f6d89d486ff
Vladimir Keda <vladimirkeda@yandex.ru>
2026-04-15 17:47:33 +0300

    Startup

---
+++ Assets/Internal_Assets/Scripts/Ecs/Systems/Startup.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using Scellecs.Morpeh;
+using Unity.AI.Navigation;
+using UnityEngine;
+using VContainer;
+
+namespace Secret
+{
+    public class Startup : MonoBehaviour
+    {
+        private World _world;
+
+        private void Start()
+        {
+            Application.targetFrameRate = 60;
+            this._world = World.Default;
+
+            var systemGroup = this._world.CreateSystemsGroup();
+
+            #region Systems
+
+
+
+            #endregion
+
+            this._world.AddSystemsGroup(order: 0, systemGroup);
+        }
+    }
+}
