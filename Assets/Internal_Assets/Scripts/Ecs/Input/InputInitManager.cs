e204a7614cf94e7e7f3660b2c5b5a49461950c2f
Vladimir Keda <vladimirkeda@yandex.ru>
2026-04-16 05:35:04 +0300

    Joystick juice

--- Assets/Internal_Assets/Scripts/Ecs/Input/InputInitManager.cs
+++ Assets/Internal_Assets/Scripts/Ecs/Input/InputInitManager.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 namespace Secret
 {
@@ -11,6 +12,8 @@ namespace Secret
         [SerializeField] private InputActionAsset _inputActions;
         private InputAction _moveAction;
         private Vector2 _moveValue;
+        [SerializeField] private Image _joystickBack;
+        [SerializeField] private Vector3 _joystickStartPoint;
 
         public InputAction MoveAction => _moveAction;
 
@@ -27,8 +30,25 @@ namespace Secret
         private void Awake()
         {
             Instance = this;
+
+            _joystickStartPoint = _joystickBack.rectTransform.localPosition;
             
             _moveAction = InputSystem.actions.FindAction("Move");
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                _joystickBack.gameObject.SetActive(true);
+                _joystickBack.transform.position = Input.mousePosition;
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                _joystickBack.gameObject.SetActive(false);
+                _joystickBack.rectTransform.localPosition = _joystickStartPoint;
+            }
         }
     }
 }
