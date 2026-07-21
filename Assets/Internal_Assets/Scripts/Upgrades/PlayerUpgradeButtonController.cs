babc8d29081cf5e630e5278bd4fd9e2e9b0c6475
Vladimir Keda <vladimirkeda@yandex.ru>
2026-07-21 19:03:43 +0300

    Players Upgrade links and behaviour

--- Assets/Internal_Assets/Scripts/Upgrades/PlayerUpgradeButtonController.cs
+++ Assets/Internal_Assets/Scripts/Upgrades/PlayerUpgradeButtonController.cs
@@ -1,6 +1,7 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Secret
 {
@@ -11,6 +12,15 @@ namespace Secret
         [SerializeField] private TextMeshProUGUI _currentValueText;
         [SerializeField] private TextMeshProUGUI _nextValueText;
         [SerializeField] private TextMeshProUGUI _mainResCostText;
+        
+        [SerializeField] private GameObject _arrowValues;
+        [SerializeField] private Button _buttonBuy;
+
+
+        public void TryBuyUpgrade()
+        {
+
+        }
 
         private void OnEnable()
         {
@@ -24,16 +34,39 @@ namespace Secret
             var currentValue = PlayerUpgradeManager.Instance.GetCurrentValueByType(_upgradeType);
             var nextValue = PlayerUpgradeManager.Instance.GetNextValueByType(_upgradeType);
 
-            _levelText.text = currentLevel + "/" + upgrade.Levels.Count;
-            _currentValueText.text = ValueStringHelper.ScoreShow(currentValue);
-            _nextValueText.text = ValueStringHelper.ScoreShow(nextValue);
+            if (nextValue >= 9999)
+            {
+                _levelText.text = "MAX";
+                _currentValueText.text = ValueStringHelper.ScoreShow(currentValue);
+                _nextValueText.text = "";
+                _arrowValues.SetActive(false);
+                _buttonBuy.gameObject.SetActive(false);
+            }
+            else
+            {
+                _levelText.text = currentLevel + "/" + upgrade.Levels.Count;
+                _currentValueText.text = ValueStringHelper.ScoreShow(currentValue);
+                _nextValueText.text = ValueStringHelper.ScoreShow(nextValue);
             
-            UpdateCost(currentLevel, upgrade);
+                UpdateCost(currentLevel, upgrade);
+            }
         }
 
         private void UpdateCost(int currentLevel, Upgrade upgrade)
         {
             _mainResCostText.text = ValueStringHelper.ScoreShow(upgrade.Levels[currentLevel].Cost.Resources[0].Value);
+            var res = PlayerResourceStats.Instance.GetResourceByType(upgrade.Levels[currentLevel].Cost.Resources[0]
+                .ResourceType);
+            if (res.Value < upgrade.Levels[currentLevel].Cost.Resources[0].Value)
+            {
+                _buttonBuy.interactable = false;
+                _mainResCostText.color = Color.red;
+            }
+            else
+            {
+                _buttonBuy.interactable = true;
+                _mainResCostText.color = Color.white;
+            }
         }
     }
 }
